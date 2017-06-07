881313b642a0ab07d99128065d2cffe8d358e04e
alex <alex@staticlibs.net>
2017-06-07 11:51:42 +0100

    dyload added

---
+++ dyload.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+define(["./wiltoncall", "./utils"], function(wiltoncall, utils) {
+    "use strict";
+
+    function loadSharedLib(options) {
+        var opts = utils.defaultObject(options);
+        var onSuccess = opts.onSuccess;
+        var onFailure = opts.onFailure;
+        delete opts.onSuccess;
+        delete opts.onFailure;
+        try {
+            wiltoncall("dyload_shared_library", opts);
+            utils.callOrIgnore(onSuccess);
+        } catch (e) {
+            utils.callOrThrow(onFailure, e);
+        }
+    }
+
+    return {
+        loadSharedLib: loadSharedLib
+    };
+});
