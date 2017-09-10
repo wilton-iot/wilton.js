0f5849433e4d53c17bcdac93f08934cf0bac2fa7
alex <alex@staticlibs.net>
2017-09-10 15:44:27 +0100

    serial module added

---
+++ serial/Serial.js
@@ -0,0 +1,69 @@
+
+define(["../wiltoncall", "../utils"], function(wiltoncall, utils) {
+    "use strict";
+
+    var Serial = function(options, callback) {
+        var opts = utils.defaultObject(options);
+        try {
+            var handleJson = wiltoncall("serial_open", opts);
+            var handleParsed = JSON.parse(handleJson);
+            this.handle = handleParsed.serialHandle;
+            utils.callOrIgnore(callback);
+        } catch (e) {
+            utils.callOrThrow(callback, e);
+        }
+    };
+
+    Serial.prototype = {
+
+        read: function(length, callback) {
+            try {
+                var res = wiltoncall("serial_read", {
+                    serialHandle: this.handle,
+                    length: length
+                });
+                utils.callOrIgnore(callback, res);
+                return res;
+            } catch (e) {
+                utils.callOrThrow(callback, e);
+            }
+        },
+
+        readLine: function(callback) {
+            try {
+                var res = wiltoncall("serial_readline", {
+                    serialHandle: this.handle
+                });
+                utils.callOrIgnore(callback, res);
+                return res;
+            } catch (e) {
+                utils.callOrThrow(callback, e);
+            }
+        },
+
+        write: function(data, callback) {
+            try {
+                wiltoncall("serial_write", {
+                    serialHandle: this.handle,
+                    data: utils.defaultString(data)
+                });
+                utils.callOrIgnore(callback);
+            } catch (e) {
+                utils.callOrThrow(callback, e);
+            }
+        },
+
+        close: function(callback) {
+            try {
+                wiltoncall("serial_close", {
+                    serialHandle: this.handle
+                });
+                utils.callOrIgnore(callback);
+            } catch (e) {
+                utils.callOrThrow(callback, e);
+            }
+        }
+    };
+
+    return Serial;
+});
