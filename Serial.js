390058b59f0f8f05f9e3528680b35c1e1be007eb
alex <alex@staticlibs.net>
2017-10-03 13:20:02 +0100

    serial apidocs

--- Serial.js
+++ Serial.js
@@ -20,7 +20,39 @@
  * __wilton/Serial__ \n
  * Connect to hardware devices using Serial protocol.
  * 
- * TODO
+ * This module allows to intract with hardware devices using `RS232` protocol.
+ * 
+ * Responses from device are returned encoded in hexadecimal encoding, use `wilton/hex` for decoding.
+ * 
+ * Underlying Serial connection can be closed manually with `close()`
+ * or it will be closed during the shutdown.
+ * 
+ * Usage example:
+ * 
+ * @code
+ * 
+ * // open connection
+ * var ser = new Serial({
+ *     port: "/dev/ttyUSB1",
+ *     baudRate: 4800,
+ *     parity: "NONE", // "EVEN", "ODD", "MARK", "SPACE"
+ *     byteSize: 8,
+ *     stopBitsCount: 1,
+ *     timeoutMillis: 500
+ * });
+ *
+ * // write request
+ * var bytes_written1 = ser.writePlain("foo\r\n");
+ * var bytes_written2 = ser.writeHex("66 6f 6f 0d 0a");
+ *
+ * // read response
+ * var respHex = ser.readLine();
+ *
+ * // close connection
+ * ser.close();
+ * 
+ * @endcode
+ * 
  */
 define([
     "./dyload",
@@ -34,6 +66,25 @@ define([
         name: "wilton_serial"
     });
 
+    /**
+     * @function constructor
+     * 
+     * Creates Serial instance.
+     * 
+     * Creates Serial object instace and opens the underlying `RS232` connection.
+     * 
+     * @param options `Object` configuration object, see possible options below
+     * @param callback `Function|Undefined` callback to receive result or error
+     * @returns `Object` Serial instance
+     * 
+     * __Options__
+     *  - __port__ `String` serial port address (OS-dependent), examples: `/dev/ttyUSB1`, `COM4`
+     *  - __baudRate__ `Number` connection baud rate
+     *  - __parity__ `String` enables parity checking, supported values: `NONE`, `EVEN`, `ODD`, `MARK`, `SPACE`
+     *  - __byteSize__ `Number` number of data bits
+     *  - __stopBitsCount__ `Number` number of stop bits
+     *  - __timeoutMillis__ `Number` timeout for read and write operations (in milliseconds)
+     */
     var Serial = function(options, callback) {
         var opts = utils.defaultObject(options);
         try {
@@ -52,7 +103,21 @@ define([
     };
 
     Serial.prototype = {
-
+        /**
+         * @function read
+         * 
+         * Reads data from device.
+         * 
+         * Tries to read a specified amount of data from the device.
+         * Returned result can conntains less data than requested.
+         * Returns empty result if no data is available and timeout is exceeded.
+         * 
+         * Uses `timeoutMillis` parameter (specified in constructor) as a timeout.
+         * 
+         * @param length `Number` amount of the data to read (in bytes)
+         * @param callback `Function|Undefined` callback to receive result or error
+         * @returns `String` device response data in hexadecimal encoding, empty string on timeout
+         */
         read: function(length, callback) {
             try {
                 var res = wiltoncall("serial_read", {
@@ -66,6 +131,22 @@ define([
             }
         },
 
+        /**
+         * @function readLine
+         * 
+         * Reads single line of data from device.
+         * 
+         * Tries to read a data from the device until line ending (`\n` or `\r\n`) will be
+         * read or timeout happens. Line ending is not included into result.
+         * 
+         * Returned result can conntains less data than requested.
+         * Returns empty result if no data is available and timeout is exceeded.
+         * 
+         * Uses `timeoutMillis` parameter (specified in constructor) as a timeout.
+         * 
+         * @param callback `Function|Undefined` callback to receive result or error
+         * @returns `String` device response data in hexadecimal encoding, empty string on timeout
+         */
         readLine: function(callback) {
             try {
                 var res = wiltoncall("serial_readline", {
@@ -78,6 +159,24 @@ define([
             }
         },
 
+        /**
+         * @function writePlain
+         * 
+         * Writes specifed string to device.
+         * 
+         * Writes specified string to device converting the bytes
+         * of input string into hexadecimal encoding as-is without changing the UTF-16 encoding.
+         * Using this method with non-ASCII symbols may lead to unexpected results.
+         * 
+         * To write UTF-8 bytes to device use `writeHex()` in conjunction with
+         * `hex.encodeUTF8()`.
+         * 
+         * Uses `timeoutMillis` parameter (specified in constructor) as a timeout.
+         * 
+         * @param data `String` string to write to device
+         * @param callback `Function|Undefined` callback to receive result or error
+         * @returns `Number` number of bytes written to device
+         */
         writePlain: function(data, callback) {
             try {
                 var resStr = wiltoncall("serial_write", {
@@ -94,6 +193,20 @@ define([
             }
         },
 
+        /**
+         * @function writeHex
+         * 
+         * Writes specifed string in hexadecimal encoding to device.
+         * 
+         * Writes specified hex-string to device, unlike `writePlain()` this
+         * function may be used to write abritrary (possibly binary) data.
+         * 
+         * Uses `timeoutMillis` parameter (specified in constructor) as a timeout.
+         * 
+         * @param dataHex `String` string encoded as hexadecimal to write to device
+         * @param callback `Function|Undefined` callback to receive result or error
+         * @returns `Number` number of bytes written to device
+         */
         writeHex: function(dataHex, callback) {
             try {
                 var resStr = wiltoncall("serial_write", {
@@ -110,6 +223,17 @@ define([
             }
         },
 
+        /**
+         * @function close
+         * 
+         * Closes connection to device
+         * 
+         * Closes Serial connection releases system resources.
+         * Devices not closed manually, will be closed automatically during the shutdown.
+         *
+         * @param callback `Function|Undefined` callback to receive result or error
+         * @returns `Undefined`
+         */
         close: function(callback) {
             try {
                 wiltoncall("serial_close", {
